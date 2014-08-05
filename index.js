671d311192d9103f75abcb4a81c5ac21612eed8a
Jennifer Fong <jen@ednapiranha.com>
2014-08-04 18:57:20 -0700

    added generate and getToken

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 'use strict';
+
+var uuid = require('uuid');
+var level = require('level');
+var ttl = require('level-ttl');
+
+var TTL_LIMIT = 1000 * 60 * 60 * 24; // 24 hours
+
+var RevisitToken = function (options) {
+  if (!options) {
+    options = {};
+  }
+
+  var dbPath = options.db || './db-tokens';
+  var limit = parseInt(options.ttl, 10) || TTL_LIMIT;
+  var frequency = parseInt(options.frequency, 10) || 10000;
+
+  var db = level(dbPath, {
+    createIfMissing: true,
+    valueEncoding: 'json'
+  });
+
+  db = ttl(db, { checkFrequency: frequency || 10000 });
+
+  this.generate = function (next) {
+    var token = uuid.v4();
+
+    db.put('token!' + token, token, {
+      ttl: limit
+    }, function (err) {
+      if (err) {
+        next(err);
+        return;
+      }
+
+      next(null, token);
+    });
+  };
+
+  this.getToken = function (token, next) {
+    db.get('token!' + token, function (err, token) {
+      if (err || !token) {
+        next(new Error('No token found'));
+        return;
+      }
+      console.log(err)
+      next(null, token);
+    });
+  };
+};
+
+module.exports = RevisitToken;
--- test/test.index.js
+++ test/test.index.js
@@ -3,3 +3,30 @@
 process.env.NODE_ENV = 'test';
 
 var should = require('should');
+var child = require('child_process');
+
+var RevisitToken = require('../index');
+var rt = new RevisitToken({
+  db: './test/db-token',
+  ttl: 500,
+  frequency: 1
+});
+
+describe('RevisitToken', function () {
+  after(function () {
+    child.exec('rm -rf ./test/db*');
+  });
+
+  it('should create a new token and expire it', function (done) {
+    rt.generate(function (err, token) {
+      should.exist(token);
+
+      setTimeout(function () {
+        rt.getToken(token, function (err) {
+          should.exist(err);
+          done();
+        });
+      }, 1500);
+    });
+  });
+});
